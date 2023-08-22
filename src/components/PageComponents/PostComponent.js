906f0a184217b0eb81c495ece52fb4fb7e2799f3
monique <moniquejp16@gmail.com>
2023-08-22 14:29:17 -0300

    fix: atualizando

--- src/components/PageComponents/PostComponent.js
+++ src/components/PageComponents/PostComponent.js
@@ -43,7 +43,7 @@ export default function Post(props) {
   const [postContent, setContent] = useState(content);
   const [hashtags, setHashtags] = useState([]);
   const [isLiked, setIsLiked] = useState(likedUserIds.includes(Number(userId)));
-  
+
   const [metadata, setMetadata] = useState({
     title: "",
     description: "",
@@ -54,22 +54,22 @@ export default function Post(props) {
   const object = { headers: { Authorization: `Bearer ${token}` } };
 
   const { setPosts, setLikes } = useContext(AuthContext);
-  const contentEdit = useCallback((inputElement)=> {
-    if(inputElement) inputElement.focus()
+  const contentEdit = useCallback((inputElement) => {
+    if (inputElement) inputElement.focus()
   }, [])
-  function getPosts(){
+  function getPosts() {
     axios
-    .get(`${process.env.REACT_APP_API_URL}/timeline`, object)
-    .then((res) => setPosts(res.data.rows))
-    .catch((err) =>alert(err.response.data))
+      .get(`${process.env.REACT_APP_API_URL}/timeline`, object)
+      .then((res) => setPosts(res.data.rows))
+      .catch((err) => alert(err.response.data))
   }
-  function getLikes(){
+  function getLikes() {
     const URL = `${process.env.REACT_APP_API_URL}/likes`;
     axios
       .get(URL, object)
-      .then((res) => {setLikes(res.data)})
+      .then((res) => { setLikes(res.data) })
       .catch((err) => alert(err.response.data));
-    }
+  }
   useEffect(() => {
     const extractedHashtags = [];
     reactStringReplace(postContent, /#(\w+)/g, (match, i) => {
@@ -79,9 +79,9 @@ export default function Post(props) {
     console.log(hashtags)
   }, [postContent]);
 
-  useEffect(()=>{
-    if ( setAtualizeHashtag ) setAtualizeHashtag(prev => !prev);
-  },[numberLikes])
+  useEffect(() => {
+    if (setAtualizeHashtag) setAtualizeHashtag(prev => !prev);
+  }, [numberLikes])
 
   useEffect(() => {
     if (token) {
@@ -134,9 +134,8 @@ export default function Post(props) {
       } else if (likes.length === 2) {
         return `${likes[0]} e ${likes[1]} curtiram`;
       } else {
-        return `${likes.slice(0, 2).join(", ")} e outros ${
-          numberLikes - 2
-        } curtiram`;
+        return `${likes.slice(0, 2).join(", ")} e outros ${numberLikes - 2
+          } curtiram`;
       }
     }
   }
@@ -218,24 +217,24 @@ export default function Post(props) {
     setIsModalOpen(false);
     Delete(postId);
   }
-  function handleKeyEvent(event){
-      if(event.key === "Escape") {
-        setIsEditing(false)
-        setContent(content)
+  function handleKeyEvent(event) {
+    if (event.key === "Escape") {
+      setIsEditing(false)
+      setContent(content)
+    }
+    if (event.key === "Enter") {
+      setLoading(true)
+      const obj = {
+        postContent,
+        hashtags
       }
-      if(event.key === "Enter") {
-        setLoading(true)
-        const obj = {
-          postContent,
-          hashtags
-        }
-        axios.patch(`${process.env.REACT_APP_API_URL}/edit/${postId}`, obj)
+      axios.patch(`${process.env.REACT_APP_API_URL}/edit/${postId}`, obj)
         .then()
         .catch(err => alert(err.response.data))
-        .finally(()=> setLoading(false))
-      }
+        .finally(() => setLoading(false))
+    }
   }
-  
+
   function Delete(id) {
     console.log(id);
     const url = `${process.env.REACT_APP_API_URL}/postDelete/${id}`;
@@ -299,32 +298,34 @@ export default function Post(props) {
           </Link>
           {Number(userId) === idUser ? (
             <div>
-              <StyledPencil data-test="edit-btn" onClick={handleEditPost}/>
+              <StyledPencil data-test="edit-btn" onClick={handleEditPost} />
               <StyledTrash data-test="delete-btn" onClick={() => setIsModalOpen(true)} />
             </div>
           ) : (
             ""
           )}
         </div>
-        {isEditign ? 
-        (<EditInput 
-          value={postContent}
-          disabled={loading} 
-          onBlur={()=> {setIsEditing(false)
-          setContent(content)}}
-          onKeyDown={handleKeyEvent}
-          onChange={(event)=> setContent(event.target.value)} 
-          ref={contentEdit}/>) : 
-        (<Lato400
-          style={{ color: "#B7B7B7", fontSize: "17px" }}
-          data-test="description">
-          {reactStringReplace(content, /#(\w+)/g, (match, i) => (
-            <span key={i} onClick={() => navigate(`/hashtag/${match}`)}>
-              {" "}
-              #{match}{" "}
-            </span>
-          ))}</Lato400>)
-          }
+        {isEditign ?
+          (<EditInput
+            value={postContent}
+            disabled={loading}
+            onBlur={() => {
+              setIsEditing(false)
+              setContent(content)
+            }}
+            onKeyDown={handleKeyEvent}
+            onChange={(event) => setContent(event.target.value)}
+            ref={contentEdit} />) :
+          (<Lato400
+            style={{ color: "#B7B7B7", fontSize: "17px" }}
+            data-test="description">
+            {reactStringReplace(content, /#(\w+)/g, (match, i) => (
+              <span key={i} onClick={() => navigate(`/hashtag/${match}`)}>
+                {" "}
+                #{match}{" "}
+              </span>
+            ))}</Lato400>)
+        }
         <a href={url} target="_blank" data-test="link">
           <SCMetadata>
             <div>
